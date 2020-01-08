b2b352560d5b50a848cb4362df2a83ea571fd68f
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2020-01-09 00:30:29 +0100

    Scafolds the App and creates folders, subfolders and files; Display the first component in the App.

---
+++ src/screens/LoggedOut.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
+
+export default class LoggedOut extends Component { 
+ render() {
+    return <Text > Hello world </Text>
+  }
+}
