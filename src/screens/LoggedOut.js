46eb7162d8c635a8a9ad93c27e0263b2e2a2a82f
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2020-01-09 01:33:12 +0100

    Changes Background and Add a Logo Image; For wrapper added background color that changes the whole screen color; For welcomeWrapper added margin and padding to handle the layout for the two-component.

--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -1,14 +1,46 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  Image,
-  TouchableHighlight,
-  ScrollView,
-} from 'react-native';
-
-export default class LoggedOut extends Component { 
- render() {
-    return <Text > Hello world </Text>
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import colors from "../styles/color";
+export default class LoggedOut extends Component {
+  render() {
+    return (
+      <View style={styles.wrapper}> 
+        <View style={styles.welcomeWrapper}>
+          <Image
+            style={styles.logo}
+            source={require("../img/airbnb-logo.png")}
+          />
+          <Text style={styles.welcomeText}>
+            Welcome to Airbnb Clone with React Native
+          </Text>
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    display: "flex",
+    backgroundColor: colors.green01 
+  },
+  welcomeWrapper: {
+    flex: 1,
+    display: "flex",
+    marginTop: 30,
+    padding: 20
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginTop: 50,
+    marginBottom: 40
+  },
+  welcomeText: {
+    fontSize: 30,
+    color: colors.white,
+    fontWeight: "300",
+    marginBottom: 40
+  }
+});
