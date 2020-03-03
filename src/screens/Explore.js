467c8b01c04293b53c159ca4c60feb925a0e1f08
Boa <boa.matule@gmail.com>
2020-03-03 22:03:57 +0100

    Implements  Airbnb Plus section and adds a featured image just bellow the Airbnb Plus Section.

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -63,6 +63,38 @@ export default class Explore extends Component {
               />
               </ScrollView>
             </View>
+
+            <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+              <Text style={{ fontSize: 24, fontWeight: "700" }}>
+                Introducing Airbnb Plus
+              </Text>
+              <Text style={{ fontWeight: "100", marginTop: 10 }}>
+                A new selection of homes verified for quality & comfort
+              </Text>
+            </View>
+
+            <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+   <Text style={{ fontSize: 24, fontWeight: "700" }}>
+     Introducing Airbnb Plus
+   </Text>
+   <Text style={{ fontWeight: "100", marginTop: 10 }}>
+     A new selection of homes verified for quality & comfort
+   </Text>
+   <View style={{ width: "100%", height: 200, marginTop: 20 }}>
+     <Image
+       style={{
+         flex: 1,
+         height: null,
+         width: null,
+         resizeMode: "cover",
+         borderRadius: 5,
+         borderWidth: 1,
+         borderColor: "#dddddd"
+       }}
+       source={require("../img/Home.jpg")}
+     />
+   </View>
+</View>
           </View>
           </SafeAreaView>
       </ScrollView>
