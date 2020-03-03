34ab91dc826ae6f20e4fe332f199d9736fd6e425
Boa <boa.matule@gmail.com>
2020-03-03 19:33:26 +0100

    Adds more styles to the search TextInput component and to the View Component wrapping the Icon and seach input to make it more appealing.

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -6,28 +6,43 @@ export default class Explore extends Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
-          <View 
+        <View
            style={{
-            backgroundColor: "white",
-            height: 80,
-            borderBottomWidth: 1,
-            borderBottomColor: "#dddddd"
-          }}
-          >
-            <Icon name="ios-search" size={20} style={{ marginRight: 10 }}/>
-            <TextInput />
-          </View>
-        </View>
+             backgroundColor: "white",
+             height: 80,
+             borderBottomWidth: 1,
+             borderBottomColor: "#dddddd"
+           }}
+         >
+           <View
+             style={{
+               flexDirection: "row",
+               padding: 10,
+               backgroundColor: "white",
+               marginHorizontal: 20,
+               shadowOffset: { width: 0, height: 0 },
+               shadowColor: "black",
+               shadowOpacity: 0.2
+             }}
+           >
+             <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+             <TextInput
+               underlineColorAndroid="transparent"
+               placeholder="Try Maputo"
+               placeholderTextColor="grey"
+               style={{ flex: 1, fontWeight: "700", backgroundColor: "white" }}
+             />
+           </View>
+         </View>
       </SafeAreaView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
