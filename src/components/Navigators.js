8332fc3b452238fe99d8f8d483b3b29df499a1d0
Boa <boa.matule@gmail.com>
2020-03-03 19:11:56 +0100

    Adds Icon with style installing react-native-vector icons and importaing the Icon from the installed packed; WIP:

--- src/components/Navigators.js
+++ src/components/Navigators.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Explore from '../screens/Explore';
@@ -6,28 +7,63 @@ import Inbox from '../screens/Inbox';
 import Trip from '../screens/Trip';
 import LoggedOut from "../screens/LoggedOut";
 import Login from "../screens/Login";
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Image} from 'react-native'
 
-const TabNavigator = createBottomTabNavigator({
-  Explore: {
-    screen: Explore,
+const TabNavigator = createBottomTabNavigator(
+  {
+    Explore: {
+      screen: Explore,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-search" color={tintColor} size={24} />
+        )
+      }
+    },
+    Saved: {
+      screen: Saved,
+      navigationOptions: {
+        tabBarLabel: "SAVED",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-heart" color={tintColor} size={24} />
+        )
+      }
+    },
+    Trip: {
+      screen: Trip,
+      navigationOptions: {
+        tabBarLabel: "TRIP",
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("../img/airbnb1.png")}
+            style={{ height: 24, width: 24, tintColor: tintColor }}
+          />
+        )
+      }
+    },
+    Inbox: {
+      screen: Inbox,
+      navigationOptions: {
+        tabBarLabel: "INBOX",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-chatboxes" color={tintColor} size={24} />
+        )
+      }
+    },
+    Profile: {
+      screen: Inbox,
+      navigationOptions: {
+        tabBarLabel: "PROFILE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person" color={tintColor} size={24} />
+        )
+      }
+    }
   },
-  Saved: {
-    screen: Saved,
-  },
-  Trip: {
-    screen: Trip,
-  },
-  Inbox: {
-    screen: Inbox,
-  },
-  Profile: {
-    screen: Inbox,
-  },
-  Login: {
-    screen: Login
-  },
-  Logout: {
-    screen: LoggedOut
-  },
-});
+);
+
+
+
+
 export default createAppContainer(TabNavigator);
