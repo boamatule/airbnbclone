21d931d01b9a05f7e2fc52c7412528cdc369fe30
Boa <boa.matule@gmail.com>
2020-03-03 20:15:43 +0100

    Imports required components into the Explore.js from react-native-package to implement the Category section; USes the ScrollView to wrap the View componenent with inline styles and uses the View to wrap the Text component containing a description with some styling. WIP

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,48 +1,54 @@
 import React, {Component} from 'react';
-import {View, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import { View, TextInput, SafeAreaView, Text, Image, ScrollView } from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
 
 export default class Explore extends Component {
   render() {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View
-           style={{
-             backgroundColor: "white",
-             height: 80,
-             borderBottomWidth: 1,
-             borderBottomColor: "#dddddd"
-           }}
-         >
-           <View
-             style={{
-               flexDirection: "row",
-               padding: 10,
-               backgroundColor: "white",
-               marginHorizontal: 20,
-               shadowOffset: { width: 0, height: 0 },
-               shadowColor: "black",
-               shadowOpacity: 0.2
-             }}
-           >
-             <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
-             <TextInput
-               underlineColorAndroid="transparent"
-               placeholder="Try Maputo"
-               placeholderTextColor="grey"
-               style={{ flex: 1, fontWeight: "700", backgroundColor: "white" }}
-             />
-           </View>
-         </View>
-      </SafeAreaView>
+      <ScrollView scrollEventThrottle={16}> 
+        <SafeAreaView style={{ flex: 1 }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              height: 80,
+              borderBottomWidth: 1,
+              borderBottomColor: "#dddddd"
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                padding: 10,
+                backgroundColor: "white",
+                marginHorizontal: 20,
+                shadowOffset: { width: 0, height: 0 },
+                shadowColor: "black",
+                shadowOpacity: 0.2
+              }}
+            >
+              <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+              <TextInput
+                underlineColorAndroid="transparent"
+                placeholder="Try Maputo"
+                placeholderTextColor="grey"
+                style={{ flex: 1, fontWeight: "700", backgroundColor: "white" }}
+              />
+            </View>
+          </View>
+        </SafeAreaView>
+
+        <View style={{ flex: 1, backgroundColor: "white", paddingTop: 20 }}>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: "700",
+                paddingHorizontal: 20
+              }}
+            >
+              What can we help you find, Boa?
+            </Text>
+          </View>
+      </ScrollView>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
